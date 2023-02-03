a4b7094d964da893799e7676c1d3e9eff32e93ef
Nikita <therederickmind@gmail.com>
2023-02-03 10:59:19 +0300

    First version, token is hidden in env

---
+++ kittybot.py
@@ -0,0 +1,43 @@
+import requests
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
+import os
+
+from dotenv import load_dotenv 
+
+load_dotenv()
+
+secret_token = os.getenv('TOKEN')
+
+updater = Updater(token=secret_token)
+URL = 'https://api.thecatapi.com/v1/images/search'
+
+def get_new_image():
+    response = requests.get(URL).json()
+    random_cat = response[0].get('url')
+    return random_cat
+
+def new_cat(update, context):
+    chat = update.effective_chat
+    context.bot.send_photo(chat.id, get_new_image())
+    print(update)
+
+def wake_up(update, context):
+    chat = update.effective_chat
+    name = update.message.chat.first_name
+    # За счёт параметра resize_keyboard=True сделаем кнопки поменьше
+    button = ReplyKeyboardMarkup([['/newcat']], resize_keyboard=True)
+
+    context.bot.send_message(
+        chat_id=chat.id,
+        text='Привет, {}. Посмотри, какого котика я тебе нашёл'.format(name),
+        reply_markup=button
+    )
+
+    context.bot.send_photo(chat.id, get_new_image())
+
+updater.dispatcher.add_handler(CommandHandler('start', wake_up))
+updater.dispatcher.add_handler(CommandHandler('newcat', new_cat))
+
+updater.start_polling()
+updater.idle()
